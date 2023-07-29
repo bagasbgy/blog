95ba85d9e5786e9b06f13d13b1d9de2290d0b867
R. Dimas Bagas Herlambang <bagasbgy@gmail.com>
2023-07-29 21:33:46 +0700

    refactor: move to vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
     trailingSlash: true,
-    images: {
-        unoptimized: true,
-    },
     webpack(config) {
         config.module.rules.push({
             test: /\.svg$/i,
