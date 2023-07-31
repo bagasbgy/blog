5472084bef3d74a0e3c9644a63fd0671ff9bb708
R. Dimas Bagas Herlambang <bagasbgy@gmail.com>
2023-07-31 18:34:13 +0700

    refactor: changed static files build strategy

--- blog/2023/07/hello-world/hello-world.template.md
+++ blog/2023/07/hello-world/hello-world.template.md
@@ -3,9 +3,8 @@ title: "Hello World!"
 author: "R. Dimas Bagas Herlambang"
 date: "2023-07-29 13:11:04+0700"
 cover:
-    src: "hello-world_files/cover.png"
+    src: "hello-world_assets/cover.png"
     alt: "Indonesian cyberpunk street market, generated using Stable Diffusion v1.5 and Cyberpunk Scene LoRA."
     width: 768
     height: 576
 ---
-
--- lib/posts.tsx
+++ lib/posts.tsx
@@ -38,13 +38,24 @@ export const getSortedPostsData = () => {
                     const { data } = matter(templateContent)
 
                     if (process.env.NODE_ENV === "development") {
-                        // TODO: Remove file dir before convert
+                        const filesDirectory = path.join(postsDirectory, year, month, dirname, `${dirname}_files`)
+                        if (fs.existsSync(filesDirectory)) {
+                            fs.rmSync(filesDirectory, { recursive: true , force: true })
+                        }
                         const tempPath = fullPath + '.temp'
                         execSync(`conda run -n blog jupyter nbconvert --to markdown ${notebookPath}`)
                         fs.copyFileSync(fullPath, tempPath)
                         fs.copyFileSync(templatePath, fullPath)
                         fs.appendFileSync(fullPath, fs.readFileSync(tempPath))
                         fs.rmSync(tempPath)
+
+                        if (fs.existsSync(filesDirectory)) {
+                            const publicDirectory = path.join('public', 'blog', year, month, dirname, `${dirname}_files`)
+                            if (fs.existsSync(publicDirectory)) {
+                                fs.rmSync(publicDirectory, { recursive: true , force: true })
+                            }
+                            fs.cpSync(filesDirectory, publicDirectory, { recursive: true })
+                        }
                     }
 
                     postsData.push({
@@ -65,15 +76,13 @@ export const getSortedPostsData = () => {
                     })
                 }
 
-                // TODO: Move file dir copy logic to notebook scope
-                // TODO: Add asset dir logic that apply to both
-                const filesDirectory = path.join(postsDirectory, year, month, dirname, `${dirname}_files`)
-                if (fs.existsSync(filesDirectory)) {
-                    const publicDirectory = path.join('public', 'blog', year, month, dirname, `${dirname}_files`)
+                const assetsDirectory = path.join(postsDirectory, year, month, dirname, `${dirname}_assets`)
+                if (fs.existsSync(assetsDirectory)) {
+                    const publicDirectory = path.join('public', 'blog', year, month, dirname, `${dirname}_assets`)
                     if (fs.existsSync(publicDirectory)) {
                         fs.rmSync(publicDirectory, { recursive: true , force: true })
                     }
-                    fs.cpSync(filesDirectory, publicDirectory, { recursive: true })
+                    fs.cpSync(assetsDirectory, publicDirectory, { recursive: true })
                 }
             })
         })
