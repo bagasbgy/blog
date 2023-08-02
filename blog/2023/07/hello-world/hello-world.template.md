2a92acb94e8c31441b61ce6454473de44767f327
R. Dimas Bagas Herlambang <bagasbgy@gmail.com>
2023-08-02 13:43:14 +0700

    refactor: sitemap now use lastmod from frontmatter

--- blog/2023/07/hello-world/hello-world.template.md
+++ blog/2023/07/hello-world/hello-world.template.md
@@ -1,7 +1,8 @@
 ---
 title: "Hello World!"
 author: "R. Dimas Bagas Herlambang"
-date: "2023-07-29 13:11:04+0700"
+date: "2023-07-29T13:11:04+07:00"
+lastmod: "2023-08-02T13:28:59+07:00"
 cover:
     src: "hello-world_assets/cover.png"
     alt: "Indonesian cyberpunk street market, generated using Stable Diffusion v1.5 and Cyberpunk Scene LoRA."
--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,30 @@
 /** @type {import('next-sitemap').IConfig} */
+matter = require('gray-matter')
+fs = require('fs')
+
 const config = {
     siteUrl: 'https://bagasbgy.dev',
+    transform: (config, url) => {
+        if (url.startsWith('/blog/')) {
+            const fileContents = fs.readFileSync(`.${url}/${url.split('/').at(-1)}.md`, 'utf8')
+            const { data } = matter(fileContents)
+            return {
+                loc: url,
+                changefreq: config.changefreq,
+                priority: config.priority,
+                lastmod: data.lastmod || data.date,
+                alternateRefs: config.alternateRefs ?? [],
+            }
+        }
+        return {
+            loc: url,
+            changefreq: config.changefreq,
+            priority: config.priority,
+            lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+            alternateRefs: config.alternateRefs ?? [],
+        }
+    },
+    changefreq: 'daily',
     generateRobotsTxt: true,
     generateIndexSitemap: false,
 }
